92d7e6096de2d159b0ab36966dde8c108fcd73ee
Jürgen Jacobsen <30883538+jurgenjacobsen@users.noreply.github.com>
2023-01-02 22:58:57 +0000

    Delete lib directory

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Data } from './utils/Types';
-import Schema from './utils/Schema';
-import { Base } from './main/Base';
-import { Database } from './main/Database';
-export { Data, Schema, Base, Database };
